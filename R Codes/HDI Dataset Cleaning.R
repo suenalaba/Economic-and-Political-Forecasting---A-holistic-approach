04d90988fb290109c43268b7d671a07b4ce12910
nichwyl <92099084+nichwyl@users.noreply.github.com>
2021-11-05 11:23:46 +0800

    Add files via upload

---
+++ R Codes/HDI Dataset Cleaning.R
@@ -0,0 +1,359 @@
+library(data.table)
+# Install psych package
+library("psych")
+library("tidyverse")
+library("mtcars")
+
+#sources
+##http://hdr.undp.org/en/countries/profiles/ARG#
+##https://ourworldindata.org/country/argentina
+##https://data.worldbank.org/country/AR
+
+####################################################################################################################Cleaning data for indexes
+
+#HDI is the geometric mean (equally-weighted) of life expectancy, education, and GNI per capita
+
+#obtain data for the 3 indexes
+edu = fread("Education index.csv",fill = TRUE,header=TRUE)
+inc = fread("Income index.csv",fill = TRUE,header=TRUE)
+lif = fread("Life expectancy index.csv",fill = TRUE,header=TRUE)
+
+#obtains Education index data
+edu$Country
+head(edu)
+eduA= edu[Country==" Argentina",]
+summary(eduA)
+if (!require("tidyverse")) install.packages("tidyverse")
+eduA=eduA %>% discard(~all(is.na(.) | . ==""))
+eduA$`HDI Rank`="Education Index"
+eduA
+
+
+#obtaining income index data
+incA = inc[Country=="Argentina",]
+incA=incA %>% discard(~all(is.na(.) | . ==""))
+summary(incA)
+incA$`HDI Rank`="Income Index"
+incA
+
+#obtaining life expectency data
+lifA = lif[Country == "Argentina",]
+lifA=lifA %>% discard(~all(is.na(.) | . ==""))
+summary(lifA)
+lifA$`HDI Rank`="Life Expectency Index"
+lifA
+
+#combining data
+temp=rbind(eduA,incA)
+clean1 = rbind(temp,lifA)
+head(clean1)
+
+names(clean1)[1]="Indexes"
+class(clean1)
+
+clean1$Country=NULL
+
+clean1
+#transposing table to have years have columns
+clean2 = t(clean1) 
+clean2
+
+head(clean2)
+
+labels(clean2)
+
+data_new <- clean2                         
+head(data_new)              
+
+data_new <- data_new[- 1, ]  
+data_new
+
+arr = c();
+y=1990
+for(x in 1:30) 
+{
+  arr[x]=y
+  y=y+1
+}
+
+class(data)
+data <- as.data.frame(data_new)
+setDT(data) 
+colnames(data) <- c("Education_Index", "Income_Index", "Life_Expectency_Index")
+data$Year = arr
+
+data
+
+sum(is.na(data)) 
+## No Nulls
+
+#ensuring data is numerical
+data <- data[ , Education_Index := as.numeric(Education_Index)]
+data <- data[ , Income_Index := as.numeric(Income_Index)]
+data <- data[ , Life_Expectency_Index := as.numeric(Life_Expectency_Index)]
+
+
+#Calculating current year HDI
+for(x in 1:30){
+  data$HDI[x]=geometric.mean(data[x,c(Education_Index,Income_Index,Life_Expectency_Index)])
+}
+colnames(data) <- c("Education_Index", "Income_Index", "Life_Expectency_Index","year","HDI")
+data
+
+write.csv(data,"C:\\NTU\\Business\\Y2S1\\Analytics\\AY21 Team Assignment and Project\\HDIdata.csv", row.names = FALSE)
+
+
+#shifting values of HDI 5 years to allow indexes(t) to point to HDI(t+5)
+data1=data
+
+shift <- function(x, n){
+  c(x[-(seq(n))], rep(NA, n))
+}
+
+data1$HDI <- shift(data1$HDI, 5)
+
+data1=data1[order(-data1$year)]
+
+data1
+
+arr = c();
+y=2024
+for(x in 1:30) 
+{
+  arr[x]=y
+  y=y-1
+}
+arr
+
+data1$pred_Year=arr
+
+data1
+
+wdata=data1[c(6:30),]
+
+wdata
+
+write.csv(wdata,"C:\\NTU\\Business\\Y2S1\\Analytics\\AY21 Team Assignment and Project\\Indexdata.csv", row.names = FALSE)
+
+
+
+
+####################################################################################################################Cleaning data for Life Expectancy predictors(Argentina Only)
+
+##greenhouse gas Emmsions per capita
+co2 = fread("ghg-emissions-per-capita.csv")
+co2
+co2a = subset(co2,Entity=="Argentina",)
+co2a
+co2a$Code=NULL
+co2a$Entity=NULL
+names(co2a)[1]="year"
+summary(co2a)
+co2a
+## Ferility rate
+fr = fread("fertility-rate-complete-gapminder.csv",fill = TRUE,header=TRUE)
+fr
+fra = subset(fr,Entity=="Argentina",)
+fra
+fra$Code=NULL
+fra$Entity=NULL
+names(fra)[1]="year"
+summary(fra)
+fra
+## public health expenditure by gdp
+phe = fread("public-health-expenditure-share-GDP-OWID.csv")
+phe
+phe1= subset(phe,Entity=="Argentina")
+phe1$Code=NULL
+phe1$Entity=NULL
+names(phe1)[1]="year"
+summary(phe1)
+phe1
+## public health expenditure per capita
+phpc = fread("out-of-pocket-expenditure-per-capita-on-healthcare.csv")
+phpc
+phpca = subset(phpc,Entity=="Argentina",)
+phpca
+phpca$Code=NULL
+phpca$Entity=NULL
+names(phpca)[1]="year"
+summary(phpca)
+phpca
+## burden of disease
+bod = fread("dalys-rate-from-all-causes.csv")
+bod
+bod1= subset(bod,Entity=="Argentina")
+bod1$Code=NULL
+bod1$Entity=NULL
+names(bod1)[2]="year"
+summary(bod1)
+bod1
+## youth mortality rate
+mr = fread("youth-mortality-rate.csv")
+mr
+mr1= subset(mr,Entity=="Argentina")
+mr1$Code=NULL
+mr1$Entity=NULL
+names(mr1)[1]="year"
+summary(mr1)
+mr1
+##hdi argentina
+hdi = fread("human-development-index.csv")
+hdi
+hdi1= subset(hdi,Entity=="Argentina")
+hdi1$Code=NULL
+hdi1$Entity=NULL
+names(hdi1)[1]="year"
+summary(hdi1)
+hdi1
+
+
+## combining tables
+co2a
+fra
+phe1
+phpca
+bod1
+mr1
+hdi1
+
+t=merge(x = co2b, y = fra, by = "year", all = TRUE)
+t=merge(x = t, y = phe1, by = "year", all = TRUE)
+t=merge(x = t, y = phpcb, by = "year", all = TRUE)
+t=merge(x = t, y = tbb, by = "year", all = TRUE)
+t=merge(x = t, y = mr1, by = "year", all = TRUE)
+t=merge(x = t, y = hdi1, by = "year", all = TRUE)
+t
+names(t)=c("year","greenhouse_gas_per_capita","public_expenditure_health_%gdp","fertiltiy_rate","oop_expenditure_healthcare_per_capita","youth_mortality_rate","burden_of_disease","hdi")
+
+
+
+##shifting hdi by 5 years
+
+t2=t
+t2$hdi <- shift(t2$hdi, 5)
+
+t2=t2[order(-t2$year)]
+
+t2$year
+t3 = t2[year<=2013]
+
+t3
+
+write.csv(t3,"C:\\NTU\\Business\\Y2S1\\Analytics\\AY21 Team Assignment and Project\\LE factors.csv", row.names = FALSE)
+
+
+####################################################################################################################Cleaning data for Life Expectancy predictors(all countries)
+##greenhouse gas Emmsions per capita
+ghpc = fread("ghg-emissions-per-capita.csv")
+ghpc
+ghpc1= ghpc
+ghpc1$Code=NULL
+names(ghpc1)[2]="year"
+summary(ghpc1)
+ghpc1
+
+##public health expenditure
+pheAC = fread("public-health-expenditure-share-GDP-OWID.csv")
+pheAC
+pheAC1= pheAC
+pheAC1$Code=NULL
+names(pheAC1)[2]="year"
+summary(pheAC1)
+pheAC1
+
+##fertility rate
+frAC = fread("fertility-rate-complete-gapminder.csv",fill = TRUE,header=TRUE)
+frAC
+frACa = frAC
+frACa
+frACa$Code=NULL
+names(frACa)[2]="year"
+summary(frACa)
+frACa
+
+##out of pocket expenditure-per capita on healthcare
+oophAC = fread("out-of-pocket-expenditure-per-capita-on-healthcare.csv")
+oophAC
+oophACa = oophAC
+oophACa
+oophACa$Code=NULL
+names(oophACa)[2]="year"
+summary(oophACa)
+oophACa
+
+##youth mortality rate
+mrAC = fread("youth-mortality-rate.csv")
+mrAC
+mrAC1= mrAC
+mrAC1$Code=NULL
+names(mrAC1)[2]="year"
+summary(mrAC1)
+mrAC1
+
+## burden of disease
+bodAC = fread("dalys-rate-from-all-causes.csv")
+bodAC
+bodAC1= bodAC
+bodAC1$Code=NULL
+names(bodAC1)[2]="year"
+summary(bodAC1)
+bodAC1
+
+##HDI
+hdiAC = fread("human-development-index.csv")
+hdiAC
+hdiAC1= hdiAC
+hdiAC1$Code=NULL
+names(hdiAC1)[2]="year"
+summary(hdiAC1)
+hdiAC1
+
+
+
+#####merging
+ghpc1
+pheAC1
+frACa
+oophACa
+mrAC1
+bodAC1
+hdiAC1
+
+
+
+table1=merge(x = ghpc1, y = pheAC1, by = c("Entity","year"), all = TRUE)
+table1=merge(x = table1, y = frACa, by = c("Entity","year"), all = TRUE)
+table1=merge(x = table1, y = oophACa, by = c("Entity","year"), all = TRUE)
+table1=merge(x = table1, y = mrAC1, by = c("Entity","year"), all = TRUE)
+table1=merge(x = table1, y = bodAC1, by = c("Entity","year"), all = TRUE)
+table1=merge(x = table1, y =hdiAC1, by = c("Entity","year"), all = TRUE)
+table1
+names(table1)=c("country","year","greenhouse_gas_per_capita","public_expenditure_health_%gdp","fertiltiy_rate","oop_expenditure_healthcare_per_capita","youth_mortality_rate","burden_of_disease","hdi")
+table1
+table1$year
+
+##shifting hdi by 5 years
+
+
+table2=table1
+table2$hdi <- shift(table2$hdi, 5)
+
+table2=table2[order(-table2$year)]
+
+table2$year
+table3 = table2[year<2013]
+
+table3
+table4=table3[complete.cases(table3),]
+table4
+
+
+write.csv(table4,"C:\\NTU\\Business\\Y2S1\\Analytics\\AY21 Team Assignment and Project\\All countries LE factors.csv", row.names = FALSE)
+
+
+
+
+table2[country=="Argentina"&year>2005]
+
+
---
+++ R Codes/HDI Linear Regression & CART.R
@@ -0,0 +1,311 @@
+library(data.table)
+         # Install psych package
+library("psych")
+
+
+#####################################################################exploration
+library(ggplot2)
+wdata = fread("Indexdata.csv")
+summary(wdata)
+wdata
+
+##boxplot
+
+ggplot(data=wdata,aes(x=year,y=Education_Index))+geom_point()+labs(title="Year vs Education_Index")
+cor(wdata$year, wdata$Income_Index)
+ggplot(data=wdata,aes(x=year,y=Income_Index))+geom_point()+labs(title="Year vs Income_Index")
+cor(wdata$year, wdata$Income_Index)
+ggplot(data=wdata,aes(x=year,y=Life_Expectency_Index))+geom_point()+labs(title="Year vs Life_Expectency_Index")
+cor(wdata$year, wdata$Income_Index)
+ggplot(data=wdata,aes(x=year,y=HDI))+geom_point()+labs(title="Year vs HDI")
+cor(wdata$year, wdata$Income_Index)
+
+##correlation of indexes to HDI in 5 years
+ggplot(data=wdata,aes(x = Education_Index,y=HDI))+geom_point()+labs(title="HDI vs Education_Index")
+cor(wdata$HDI, wdata$Education_Index)
+
+ggplot(data=wdata,aes(x = Income_Index,y=HDI))+geom_point()+labs(title="HDI vs Income_Index")
+cor(wdata$HDI, wdata$Income_Index)
+
+ggplot(data=wdata,aes(x = Life_Expectency_Index,y=HDI))+geom_point()+labs(title="HDI vs Life_Expectency_Index")
+cor(wdata$HDI, wdata$Life_Expectency_Index)
+
+library(corrplot)
+corrplot(cor(wdata), type = "upper")
+
+
+
+
+#############################################################Regression
+
+#uses all data in set to generate regression, aim to predict 2015-2019
+
+wdata
+wdata1= wdata[c(6:25),] #use date before 2010
+wdata2 = wdata[c(1:5),] #values for 2010-2014 to predict 2015-2019
+wdata1
+wdata2
+
+
+m1 <- lm(HDI ~ Education_Index+Income_Index+Life_Expectency_Index, data = wdata1)
+summary(m1)
+
+library(car)
+vif(m1) #colinearity present between life expectancy and education, hence remove education index
+
+m2 <- lm(HDI ~ Life_Expectency_Index+Income_Index, data = wdata1)
+summary(m2) #only life expectancy index significant, so only use that as predictor
+vif(m2)
+
+
+m3 <- lm(HDI ~ Life_Expectency_Index, data = wdata1)
+summary(m3)
+
+par(mfrow = c(2,2))
+plot(m3)
+par(mfrow = c(1,1))
+
+
+test.hdi=predict(m3,newdata=wdata2) #using 2010-2014 data
+error=wdata2$HDI-test.hdi
+
+RMSE <- sqrt(mean(error^2))
+summary(abs(error))
+
+RMSE
+##Only 1 variable, but we learn that health is the most signifacnt factor in predicting HDI in 5 years, hence now we find predictos
+##that might affect LE and use that to predict HDI
+
+#################################################using LE factors as predictors
+data = fread("HDIdata.csv")
+names(data)[4]="year"
+dataLE = data
+dataLE$Education_Index=NULL
+dataLE$Income_Index=NULL
+dataLE$Life_Expectency_Index=NULL
+dataLE
+
+LE = fread("LE factors.csv")
+LE$hdi=NULL
+
+LE=merge(x = LE, y = dataLE, by = "year", all = TRUE)
+LE
+
+LE1 = LE[!is.na(LE$HDI), ]   
+LE2=LE1
+LE2$HDI= shift(LE1$HDI, 5)
+LE2
+
+LE3=LE2[order(-LE2$year)]
+
+
+LE4=LE3[year<2014,]
+LE4 = LE4[!is.na(LE4$HDI), ]  
+LE4
+
+
+
+LE4
+
+
+#####################################################full data 5 years project(Argentina only)
+
+library(rpart)
+library(rpart.plot)	
+LE4
+var = LE4[c(1:5),] #to be used as variables to predict 2014-2018
+var
+
+LE5=LE4[c(6:24),]#remainding years to be used in model
+LE5
+
+#use cart to predict
+cart11 <- rpart(HDI ~ .-year, data = LE5, method = 'anova', control = rpart.control(minsplit = 2, cp = 0))
+
+plotcp(cart11)
+
+print(cart11)
+
+CVerror.cap <- cart11$cptable[which.min(cart11$cptable[,"xerror"]), "xerror"] + cart11$cptable[which.min(cart11$cptable[,"xerror"]), "xstd"]
+
+i <- 1; j<- 4
+while (cart11$cptable[i,j] > CVerror.cap) {
+  i <- i + 1
+}
+cp.opt = ifelse(i > 1, sqrt(cart11$cptable[i,1] * cart11$cptable[i-1,1]), 1)
+
+cart22 <- prune(cart11, cp = cp.opt)
+printcp(cart22, digits = 3)
+
+
+print(cart22)
+
+rpart.plot(cart22, nn = T, main = "Optimal Tree in predicting HDI")
+## The number inside each node represent the mean value of Y.
+
+cart22$variable.importance
+
+summary(cart22)
+
+
+test.predict = predict(cart22, newdata = var)
+test.predict
+testset.error <- var$HDI - test.predict
+testset.error
+
+# Testset Errors
+RMSE.m5.test <- sqrt(mean(testset.error^2))
+summary(abs(testset.error))
+
+RMSE.m5.test
+
+
+
+######################################################################### 1 year project
+LE1y=LE1
+
+LE1y$HDI= shift(LE1$HDI, 1)
+LE1y
+
+LE1y1=LE1y[order(-LE1y$year)]
+LE1y1
+
+LE1y2=LE1y1[year<2014,]
+LE1y2 = LE1y2[!is.na(LE4$HDI), ]  
+LE1y2
+
+
+LE6=LE1y2[c(2:24),]
+LE6
+var1 = LE1y2[1,]
+var1
+
+cart111 <- rpart(HDI ~ .-year, data = LE6, method = 'anova', control = rpart.control(minsplit = 2, cp = 0))
+
+plotcp(cart111)
+
+print(cart111)
+
+CVerror.cap <- cart111$cptable[which.min(cart111$cptable[,"xerror"]), "xerror"] + cart111$cptable[which.min(cart111$cptable[,"xerror"]), "xstd"]
+
+i <- 1; j<- 4
+while (cart111$cptable[i,j] > CVerror.cap) {
+  i <- i + 1
+}
+cp.opt = ifelse(i > 1, sqrt(cart111$cptable[i,1] * cart111$cptable[i-1,1]), 1)
+
+cart222 <- prune(cart111, cp = cp.opt)
+printcp(cart222, digits = 3)
+
+
+print(cart222)
+
+rpart.plot(cart222, nn = T, main = "Optimal Tree in predicting HDI")
+## The number inside each node represent the mean value of Y.
+
+cart222$variable.importance
+
+summary(cart222)
+
+
+
+
+test.predict1 = predict(cart222, newdata = var1)
+testset.error1 <- var1$HDI - test.predict1
+
+# Testset Errors
+RMSE.m5.test1 <- sqrt(mean(testset.error1^2))
+summary(abs(testset.error1))
+
+RMSE.m5.test1
+
+
+
+#####################################comparing the 3 projections
+
+RMSE
+RMSE.m5.test
+RMSE.m5.test1
+
+
+
+
+
+######################################################################### All countries
+
+AC1 = fread("All countries LE factors.csv")
+library(caTools)
+set.seed(2004)
+AC1
+
+
+
+AC=AC1
+AC$country=NULL
+AC$year=NULL
+AC
+
+ggplot(data=AC1,aes(x=greenhouse_gas_per_capita,y=hdi))+geom_point()+labs(title="Year vs Education_Index")
+
+summary(AC)
+
+corrplot(cor(AC), type = "upper")
+
+
+
+
+# 70% trainset. Stratify on Y = mpg. Caution: Sample size only 32 in this example.
+train <- sample.split(Y = AC$hdi, SplitRatio = 0.7)
+trainset <- subset(AC, train == T)
+testset <- subset(AC, train == F)
+
+# Checking the distribution of Y is similar in trainset vs testset.
+summary(trainset$hdi)
+summary(testset$hdi)
+
+# Develop model on trainset
+mAC <- lm(hdi~.,data = trainset)
+summary(mAC)
+residuals(mAC) 
+
+vif(mAC)
+
+
+par(mfrow = c(2,2))
+plot(mAC)
+par(mfrow = c(1,1))
+
+
+
+# Residuals = Error = Actual mpg - Model Predicted mpg
+RMSE.mAC.train <- sqrt(mean(residuals(mAC)^2))  # RMSE on trainset based on mAC model.
+summary(abs(residuals(mAC)))  # Check Min Abs Error and Max Abs Error.
+
+# Apply model from trainset to predict on testset.
+predict.mAC.test <- predict(mAC, newdata = testset)
+testset.error <- testset$hdi - predict.mAC.test
+
+# Testset Errors
+RMSE.mAC.test <- sqrt(mean(testset.error^2))
+summary(abs(testset.error))
+
+RMSE.mAC.train 
+RMSE.mAC.test
+
+
+
+AC1[country=="Argentina"&year>=2008]
+
+
+pred1=AC1[country=="Argentina"&year>=2010]
+pred1 
+pred.hdi = predict(mAC,newdata=pred1)
+pred.hdi
+
+errorAC=pred1$hdi-pred.hdi
+RMSE_p<- sqrt(mean(errorAC^2))
+RMSE_p #using all countries vastly disprove accuracy of model
+
+
+
+
